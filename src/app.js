53a6f7936fb6481d30728cd2b4bccbc6d86e2f43
PK Mishal <mishalpk02@gmail.com>
2024-05-08 19:48:17 +0530

    boiler plate created

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+    console.log("hello");
+    res.render('login');
+});
+module.exports = app
